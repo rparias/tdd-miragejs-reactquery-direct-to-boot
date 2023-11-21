e1a757a9ea25d78b682899e12b7d764006252b92
Ronald Arias <ariasron@hotmail.com>
2023-11-21 13:24:57 -0500

    installs axios and miragejs to simulate http requests on tests

--- src/DirectToBoot.test.tsx
+++ src/DirectToBoot.test.tsx
@@ -1,14 +1,51 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { DirectToBoot } from './DirectToBoot'
+import { createServer, Request, Server } from 'miragejs'
+import Schema from 'miragejs/orm/schema'
+import { AnyRegistry } from 'miragejs/-types'
+
+let server: Server
 
 describe('Direct to boot', () => {
+  beforeEach(() => {
+    server = createServer({
+      routes() {
+        this.get('/api/orders/:id', (schema: Schema<AnyRegistry>, request: Request) => {
+          return {
+            id: request.params.id,
+            status: 'ready'
+          }
+        })
+      },
+    })
+  })
+  
+  afterEach(() => {
+    server.shutdown()
+  })
+
   it('renders a section for direct to boot', () => {
     render(<DirectToBoot orderId='order-id' />)
+
     expect(screen.getByRole('heading', { level: 2, name: /direct to boot/i })).toBeInTheDocument()
     expect(screen.getByText(/we are preparing your order.../i)).toBeInTheDocument()
     
     const button = screen.getByRole('button')
     expect(button).toBeInTheDocument()
     expect(button).toBeDisabled()
+  })
+  
+  it('enables button when the order is ready', async () => {
+    render(<DirectToBoot orderId='order-id' />)
+
+    expect(screen.getByRole('heading', { level: 2, name: /direct to boot/i })).toBeInTheDocument()
+    expect(screen.getByText(/we are preparing your order.../i)).toBeInTheDocument()
+    
+    const button = screen.getByRole('button')
+    expect(button).toBeInTheDocument()
+    expect(button).toBeDisabled()
+
+    await waitFor(() => expect(button).toBeEnabled())
+    await screen.findByText(/please click the button when you have arrived. one of our friendly staff will bring your order to you./i)
   })
 })
--- src/DirectToBoot.tsx
+++ src/DirectToBoot.tsx
@@ -1,9 +1,23 @@
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+
 export const DirectToBoot = ({ orderId }: { orderId: string; }) => {
+  const [status, setStatus] = useState<string>('initialized')
+
+  useEffect(() => {
+    axios.get(`/api/orders/${orderId}`)
+      .then((response: AxiosResponse<any>) => {
+        if(response.data.status === 'ready') {
+          setStatus('ready')
+        }
+      })
+  }, [orderId])
+
   return (
     <div>
       <h2>Direct To Boot</h2>
-      <p>We are preparing your order...</p>
-      <button disabled>I'm here</button>
+      <p>{status === 'ready' ? 'Please click the button when you have arrived. One of our friendly staff will bring your order to you.' : 'We are preparing your order...'}</p>
+      <button disabled={status !== 'ready'}>I'm here</button>
     </div>
   );
 };
